7a5fdbad88d31f3b83907a40a873237979d3b3a5
Eric Ballard <ericballard7@gmail.com>
2023-11-19 19:56:07 -0500

    + Filters

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,47 +1,80 @@
 import '../styles/Shop.css'
 
 import { BsSearch } from "react-icons/bs";
+import { IoFilter } from "react-icons/io5";
 
+
+import { useState } from 'react'
 
 const CATEGORIES = [
-    'face', 'body', 'lips', 'eyes'
+    'Face', 'Lips', 'Eyes', 'Body'
 ]
 
 const Shop = () => {
+
+    const [isOptionsOpen, setOptionsOpen] = useState(false)
 
     return (
         <div className="shop">
 
             <div className="filter-holder">
 
-
-                <div className="category-holder">
-
-                    <div className="categories">
-
-                        {CATEGORIES.map(type => {
-                            return (
-                                <div className="filter" key={type}>
-
-                                    <img alt='Filter Icon' src={`/images/${type}-light-icon.png`}></img>
-
-                                </div>
-                            )
-                        })}
-
+                <div className="ftoggle-holder">
+                    <div className="ftoggle" onClick={() => setOptionsOpen(!isOptionsOpen)}>
+                        <IoFilter className='filter-icon' />
                     </div>
-
                 </div>
 
                 <div className="search-holder">
-                    <input type='text'></input>
+                    <input type='text' />
 
                     <BsSearch className='search-icon' />
 
                 </div>
             </div>
 
+            <div className="item-holder">
+                <div className={"filter-options-holder" + (isOptionsOpen ? ' active' : '')}>
+                    <div className="filter-types">
+                        <div className="filter-title">
+                            <h4 >CATEGORY</h4>
+                        </div>
 
+                        <div className="filter-title">
+                            <h4>PRICE</h4>
+                        </div>
+
+                        <div className="filter-sub category">
+
+                            {CATEGORIES.map(category => {
+                                return (
+                                    <div className="filter-option-category">
+                                        <input type="checkbox" id={category} name={category} defaultChecked />
+                                        <div className="cbox-lbl-wrapper">
+                                            <label for={category}>{category}</label>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+
+                        <div className="filter-sub">
+                            <div className="range-holder">
+                                <input type="range" className='price-range' />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className={"item-list" + (isOptionsOpen ? ' inactive' : '')}>
+                    <div className="item" />
+                    <div className="item" />
+
+                    items
+                </div>
+
+            </div>
         </div>
     )
 }
